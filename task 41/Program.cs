be209d27902778141564d53951875250f29a127e
Gherman Andrian <andriangherman@gmail.com>
2023-06-06 22:31:43 +0300

    homework finished

---
+++ task 41/Program.cs
@@ -0,0 +1,15 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.    0, 7, 8, -2, -2 -> 2     1, -7, 567, 89, 223-> 3
+
+int CheckPositiveNumbers(int[] array)
+{
+    int count = 0;
+    foreach (int el in array)
+    {
+        if (el > 0) count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Введите числа через пробел: ");
+int[] userArray = Console.ReadLine()!.Split(' ').Select(e => Convert.ToInt32(e)).ToArray();
+Console.WriteLine($"массив введенный пользователем: {String.Join(" ", userArray)} количество положительных чисел: {CheckPositiveNumbers(userArray)}");
---
+++ task 43/Program.cs
@@ -0,0 +1,27 @@
+﻿//  Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine()!;
+    double result = Convert.ToDouble(value);
+    return result;
+}
+
+Console.WriteLine("Введите данные прямых (y=kx+b)");
+double k1 = Prompt("Введите угол наклона первой прямой ");
+double k2 = Prompt("Введите угол наклона второй прямой ");
+double b1 = Prompt("Введите значение y-пересечения первой прямой ");
+double b2 = Prompt("Введите значение y-пересечения второй прямой ");
+
+if (k1 == k2)
+{
+    Console.WriteLine("Прямые параллельны");
+}
+else
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine("Точка пересечения: ({0}, {1})", x, y);
+}
